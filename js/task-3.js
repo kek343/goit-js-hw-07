2b41cf4edda9cdf14b5674bafbc0775866dc006c
Olexandr Gul <142175177+kek343@users.noreply.github.com>
2024-01-09 17:20:11 +0200

    hw-7

---
+++ js/task-3.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const nameInput = document.getElementById("name-input");
+    const nameOutput = document.getElementById("name-output");
+  
+    nameInput.addEventListener("input", function() {
+      const enteredName = nameInput.value.trim();
+  
+      if (enteredName === "") {
+        nameOutput.textContent = "Anonymous";
+      } else {
+        nameOutput.textContent = enteredName;
+      }
+    });
+  });
+  
--- js/task-6.js
+++ js/task-6.js
@@ -1,5 +1,40 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
-    .padStart(6, 0)}`;
+    .padStart(6, '0')}`;
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+  const input = document.querySelector('input');
+  const createBtn = document.querySelector('[data-create]');
+  const destroyBtn = document.querySelector('[data-destroy]');
+  const boxesContainer = document.querySelector('#boxes');
+
+  createBtn.addEventListener('click', function () {
+    const amount = parseInt(input.value);
+
+    if (amount > 0 && amount <= 100) {
+      createBoxes(amount);
+      input.value = '';
+    }
+  });
+
+  destroyBtn.addEventListener('click', destroyBoxes);
+
+  function createBoxes(amount) {
+    const baseSize = 30;
+
+    for (let i = 0; i < amount; i++) {
+      const size = baseSize + i * 10;
+      const box = document.createElement('div');
+      box.style.width = `${size}px`;
+      box.style.height = `${size}px`;
+      box.style.backgroundColor = getRandomHexColor();
+      boxesContainer.appendChild(box);
+    }
+  }
+
+  function destroyBoxes() {
+    boxesContainer.innerHTML = '';
+  }
+});
